3b8fea9483f837f0fda21ae73c92df4666338b85
kojot <wojtek@herasoft.pl>
2019-09-24 09:36:34 +0200

    clean javascript project

--- javascript/src/views/Home.vue
+++ javascript/src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Welcome in the RSocket JavaScript tester
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "home",
+  components: {}
+};
 </script>
